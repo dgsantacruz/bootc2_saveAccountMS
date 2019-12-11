35ec19de9e145c170d0552f73ea3dcefaadcc7fc
Jaeggler <gonzalo.santacruzm@gmail.com>
2019-12-11 17:34:44 -0500

    Added Swagger

--- src/main/java/com/everis/savingaccount/Controller/savingAccountController.java
+++ src/main/java/com/everis/savingaccount/Controller/savingAccountController.java
@@ -8,17 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.everis.savingaccount.Model.SavingAccount;
 
 import reactor.core.publisher.Flux;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+
+@RestController()
+@RequestMapping("/savingacc")
 public class savingAccountController
 {
     @Autowired
     private SavingAccountService service;
 
     //Get all accounts
+    @GetMapping("/account/all")
     public Flux<SavingAccount> getAllAccounts()
     {
         return service.findAllAccounts();
+    }
+
+    @GetMapping("/test")
+    public String testing()
+    {
+        return "Test";
     }
 
     //Get account by number
